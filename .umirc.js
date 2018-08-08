59103d5ae3a7b7bb8ea50919dc4d3a82e6b6bf85
FattyPanda <1076659852@qq.com>
2018-08-08 17:04:29 +0800

    更新 typescript 开发配置

--- .umirc.js
+++ .umirc.js
@@ -1,6 +1,11 @@
+import { resolve } from 'path';
+
 export default {
   plugins: [
     ['umi-plugin-dva', { immer: true }],
     'umi-plugin-es5-imcompatible-versions'
   ],
+  alias: {
+    src: resolve(__dirname, 'src'),
+  }
 }
